11a24ab54b5534579284f99bcbb26cbfa7e51972
kenan324 <kenan.begic.2002@gmail.com>
2025-11-21 18:35:35 +0100

    feat: add save and mailto functionality
    Implemented save modal with a dropdown menu of
    options to save ass: "pdf", "jpeg" and a option to
    "open user email". Added classes to button because
    of better handling css button selector.
    Tweaked css button style in modal button.

--- Projekat_1/studentGames/miniKanban/js/miniKanban.js
+++ Projekat_1/studentGames/miniKanban/js/miniKanban.js
@@ -21,7 +21,6 @@ document.getElementById("modalCancel").addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-
 // Create Task
 function createTask(text) {
     const task = document.createElement("div");
@@ -49,7 +48,7 @@ document.querySelectorAll(".taskList").forEach(list => {
     });
 });
 
-// Clear board
+// Clear board Modal
 const clearModal = document.getElementById("clearModal");
 
 document.getElementById("clearBoardBtn").addEventListener("click", () => {
@@ -67,16 +66,41 @@ document.getElementById("clearNo").addEventListener("click", () => {
     clearModal.style.display = "none";
 });
 
-// Close modal
-window.addEventListener("click", e => {
-    if (e.target === clearModal) {
-        clearModal.style.display = "none";
+// Save board modal
+const saveModal = document.getElementById("saveModal");
+
+document.getElementById("saveBoardBtn").addEventListener("click", () => {
+     saveModal.style.display = "block";
+
+});
+
+// Cancel save modal button
+document.getElementById("modalSafeCancel").addEventListener("click", () =>{
+    saveModal.style.display = "none";
+})
+
+// Button for dropdown menu
+const saveMenu = document.getElementById("saveMenu");
+const dropdownMenuBtn = document.getElementById("dropdownMenuBtn");
+
+document.getElementById("dropdownMenuBtn").addEventListener("click", () => {
+    saveMenu.style.display = 
+        (saveMenu.style.display === "block") ? "none" : "block";
+});
+// Close the dropdownMenu it was interacted outside the Menu 
+document.getElementById("saveModal").addEventListener("click", e => {
+    if (!saveMenu.contains(e.target) && e.target !== dropdownMenuBtn) {            
+        saveMenu.style.display = "none";
     }
 });
 
-// Save board to PNG
-document.getElementById("saveBoardBtn").addEventListener("click", () => {
-    html2canvas(document.body).then(canvas => {
+// Button for saving to jpeg 
+document.getElementById("SaveToJpeg").addEventListener("click", () => {
+        //close the event modal
+        saveModal.style.display = "none";
+        saveMenu.style.display = "none";
+
+        html2canvas(document.body).then(canvas => {
         const link = document.createElement("a");
         link.download = "kanban_board.png";
         link.href = canvas.toDataURL();
@@ -84,13 +108,90 @@ document.getElementById("saveBoardBtn").addEventListener("click", () => {
     });
 });
 
-// Dynamically load html2canvas
-const script = document.createElement("script");
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js";
-document.body.appendChild(script);
+//Button for saving in PDF format
+document.getElementById("SaveToPdf").addEventListener("click", () => {
+    saveModal.style.display = "none";
+    saveMenu.style.display = "none";
+    // Try to use jsPDF method to save a pdf
+    html2canvas(document.getElementById('content')
+        //Add sharper compression
+        ,{ scale: 2 }).then(canvas => {
+        
+        const imgData = canvas.toDataURL('image/png');
+        const { jsPDF } = window.jspdf; // declare the jsPDF lib
+        const pdf = new jsPDF(
+            { unit: 'in', format: 'letter', orientation: 'portrait' });
+        
+        
+        pdf.addImage(imgData, 'PNG', 0, 0, 8.5, 5,);
+        pdf.save("miniKanban.pdf");
+    });
+    
+});
+// This is a more easer option
+/*
+const element = document.getElementById('content');
+    const options = {
+    margin: 1,
+    filename: 'miniKanban.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf().set(options).from(element).save();
+*/
 
+const gmailModal = document.getElementById("gmailModal");
+document.getElementById("SaveToGmail").addEventListener("click", () => {
+    saveModal.style.display = "none";
+    gmailModal.style.display = "block";
+});
+
+document.getElementById("gmailCancelBtn").addEventListener("click", () => {
+    gmailModal.style.display = "none";
+});
+
+
+
+document.getElementById("modalSend").addEventListener("click", () => {
+    //Gmail input 
+    const gmailInput = document.getElementById("Gmail");
+    gmailInput.value.trim();//remove spaces
+ 
+    const regex = new RegExp("^[a-zA-Z0-9._/-]{3,20}@gmail\.com$");//Basic regex    
+
+    if (regex.test(gmailInput.value)){sendGmail(gmailInput);};
+    gmailInput.value = '';
+});
+
+function sendGmail(g){
+    //Tried to make a function to attach a file to mailto method
+    /*
+    html2canvas(document.getElementById('content')
+        //Add sharper compression
+        ,{ scale: 2 }).then(canvas => {
+        
+        const imgData = canvas.toDataURL('image/png');
+    });
+    */
+    
+    window.location.href = `mailto:${g.value}?`;
+    //subject=${encodeURIComponent("miniKanbanJpeg")}?
+    //&body=${encodeURIComponent("imgData")}
+}
+
+// Dynamically load html2canvas and PDF
+const scriptHtml2canvas = document.createElement("script");
+scriptHtml2canvas.src = "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js";
+document.body.appendChild(scriptHtml2canvas);
+
+const scriptPdf = document.createElement("script");
+scriptPdf.src = "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+document.body.appendChild(scriptPdf);
+
+// Close modal if it was interacted outside the modal 
 window.addEventListener("click", e => {
-    if (e.target === modal) {
-        modal.style.display = "none";
+    if (e.target.classList.contains("modal")) {
+         e.target.style.display = "none";
     }
 });
