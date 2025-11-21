9a28cc3fad6e0afa23a86c49e2821f55798d8c74
kenan324 <kenan.begic.2002@gmail.com>
2025-11-21 21:36:28 +0100

    feat: add student fun zone page
    Student fun zone has a several mini games that the
    student could play and interact in.

--- Projekat_1/index.php
+++ Projekat_1/index.php
@@ -1,16 +1,39 @@
 <!-- index.php -->
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home Page</title>
-  <link rel="stylesheet" href="css\stylesheet.css">
-</head>
-<body>
-  <h1>Vision Board</h1>
-  
-
-  <label ><a href="visionBoard.html">Go to Vision Board</a></label>
-</body>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Kursevi IPI</title>
+		<link rel="stylesheet" href="css/stylesheet.css" />
+    </head>
+    <body>
+        <header>
+			<a href="index.html">
+				<img src="resources/logo-ipi.png" alt="Kursevi IPI">
+			</a>
+			<nav>
+				<ul>
+					<li><a href="index.html">O kursevima</a></li>
+					<li><a href="#">Popis kurseva</a></li>
+					<li><a href="#">Raspored kurseva</a></li>
+					<li><a href="studentFunTime.html">Student Fun Zone</a></li>
+					<li><a href="kontakt.html">Kontakt</a></li>
+				</ul>
+			</nav>
+		</header>
+		<article>
+		<h2>Student Fun Zone</h2>
+        <p>Odaberite jednu igru!</p>
+	
+        <ul>
+            <li><a href="vjezba 6/Vjezba 1.1/bingo.html">Bingo</a></li>
+            <li><a href="/vjezba 6/Vjezba 1.2/KvizVjezba.html">Kviz</a></li>
+            <li><a href="studentGames/interactiveWhiteboard">Whiteboard</a></li>
+            <li><a href="studentGames/miniKanban">Kanban</a></li>
+            <li><a href="studentGames/visionBoard">Vision Board</a></li>
+        </ul>
+        
+        </article>
+        <footer>&copy; Internacionalna poslovno-informaciona akademija Tuzla</footer>
+    </body>
 </html>
